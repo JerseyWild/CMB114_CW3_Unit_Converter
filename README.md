a5661505a305436fc6cc3156fe97124fffc4ca54
Felix Plasser <f.plasser@lboro.ac.uk>
2024-03-14 16:25:35 +0000

    Set up basic project template

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# CMB114_team-project
+# CMB114 Team Project
 Template for a simple team project
+
+The code is divided between folders `student1` and `student2`.
---
+++ student1/featureA.py
@@ -0,0 +1,9 @@
+"""
+Example code that contains a subroutine.
+"""
+
+def hello_world():
+    """
+    This routine says hello to the world.
+    """
+    print("Hello World!")
