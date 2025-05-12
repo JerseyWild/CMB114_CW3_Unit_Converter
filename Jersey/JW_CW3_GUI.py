8ec0c1aa90412365e532c8f510378832d81ec84d
cmjw <cmjw@lnx-wpt008-13.lunet.lboro.ac.uk>
2025-05-12 13:43:18 +0100

    started GUI - on button click opens a new window for unit conversions

--- Jersey/JW_CW3_GUI.py
+++ Jersey/JW_CW3_GUI.py
@@ -6,23 +6,36 @@ Created on Wed May  7 16:18:24 2025
 @author: cmjw
 """
 
+from tkinter import *
 
-import tkinter as tk
 
 
-root = tk.Tk()
-root.title("Unit Converter")
-root.geometry("800x500")
+
+def open_converter_window():
+     converter_window = Toplevel(root)
+     converter_window.title("Unit Converter - Conversions Page")
+     converter_window.geometry("800x500+1000+500")
+
+    
+root = Tk()
+root.title("Unit Converter - Home Page")
+root.geometry("800x500+1000+500")
 root.maxsize(800, 500)
-title_label = tk.Label(root, text="Unit Converter")
+title_label = Label(root, text="Unit Converter")
 
-enter_button = tk.Button(root, text="ENTER THE PROGRAM", padx=20, pady=10, activebackground="white", activeforeground="grey", anchor="center", bg="green", fg="white", font=("Century Gothic", 12), command=)
+
+
+
+enter_button = Button(root, text="ENTER THE PROGRAM", padx=20, pady=10, activebackground="white", activeforeground="grey", anchor="center", bg="green", fg="white", font=("Comic Sans MS", 12), command=open_converter_window)
 enter_button.place(x=300, y=175)
 
-enter_button = tk.Button(root, text="EXIT", padx=28.5, pady=10, activebackground="white", activeforeground="grey", anchor="center", bg="red", fg="white", font=("Century Gothic", 12))
+
+
+enter_button = Button(root, text="EXIT", padx=28.5, pady=10, activebackground="white", activeforeground="grey", anchor="center", bg="red", fg="white", font=("Comfortaa", 12), command=root.destroy)
 enter_button.place(x=300, y=250)
 
-converter = tk.Frame(root)
+
+
 
 root.mainloop()
 
