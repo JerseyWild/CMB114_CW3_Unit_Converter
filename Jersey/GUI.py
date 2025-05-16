705bc3f31176529137d93b512d5fed3eff3ba4ba
Jersey Wilden <j.wilden-23@student.lboro.ac.uk>
2025-05-16 20:40:36 +0100

    Dropbox: fixed default option so that the default text shows in the box, increased size | Added a Submit Button: when clicked, stores the current value in the dropbox, outputs a Label containing the current submission choice in a vertical list format

--- Jersey/GUI.py
+++ Jersey/GUI.py
@@ -9,19 +9,41 @@ Created on Wed May  7 16:18:24 2025
 from tkinter import *
 from tkinter import ttk
 
+vals_submitted = 0 # records how many values from the dropbox have been submitted
+
 
 def open_converter_window():
      converter_win = Toplevel(root)
      converter_win.title("Unit Converter - Conversions Page")
      converter_win.geometry(f"800x500+{center_x}+{center_y}")
-     combobox_options = StringVar()
-     thermo_val_dropdown = ttk.Combobox(converter_win, textvariable=combobox_options)
+     
+##     output_log = ttk.PanedWindow(orient=HORIZONTAL)
+##     left_list = Listbox(converter_win)
+##     left_list.pack(side=LEFT)
+##     output_log.add(left_list)
+##     right_list = Listbox(converter_win)
+##     right_list.pack(side=RIGHT)
+##     output_log.add(right_list)
+##     output_log.pack(fill=BOTH, expand=True)
+
+     def thermo_val_submitted():
+          global vals_submitted
+          print("val count =", vals_submitted)
+          log_text = "Value chosen: " + thermo_val_choice.get()
+          output_log = Label(converter_win, text=log_text) # creates a label containing the current submission choice
+          output_log.place(x=450,y=(95+vals_submitted*20)) # places the label a factor of 20 below the previous entry
+          vals_submitted += 1 # adds 1 to the count since 1 more submission has been made
+
+     thermo_val_choice = StringVar()
+     thermo_val_dropdown = ttk.Combobox(converter_win, font=(10), textvariable=thermo_val_choice)
      thermo_val_dropdown["values"] = ("Choose a value", "Energy", "Frequency", "Wavelength", "Wavenumber","Time", "Mass", "Velocity", "Momentum", "Charge", "Voltage")
      thermo_val_dropdown.set("Choose a value")
-     thermo_val_dropdown.place(x=100, y=100)
+     thermo_val_dropdown.place(x=100, y=100)   
      
+     thermo_val_button = Button(converter_win, text="Submit", font=(8), padx=0, pady=0, command=thermo_val_submitted)
+     thermo_val_button.place(x=350, y=95)
 
-  
+
 root = Tk()
 root.title("Unit Converter - Home Page")
 screen_w = root.winfo_screenwidth()
@@ -47,8 +69,3 @@ options_button = Button(root, text="OPTIONS", font=("Comic Sans MS", 12), anchor
 options_button.place(x=335, y=325)
 
 root.mainloop()
-
-
-
-
-
