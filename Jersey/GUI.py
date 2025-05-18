725b3cc9e8aa530a7818997e06cc92d621ec4b9c
Jersey Wilden <j.wilden-23@student.lboro.ac.uk>
2025-05-18 22:14:51 +0100

    Converter page: added 2 more dropdown boxes for the number and the units | Options page: added some text about the program | Converter and options pages: added a GO BACK button at the bottom of each page which when clicked deletes the current window, returning the user to the Home page

--- Jersey/GUI.py
+++ Jersey/GUI.py
@@ -9,7 +9,22 @@ Created on Wed May  7 16:18:24 2025
 from tkinter import *
 from tkinter import ttk
 
-vals_submitted = 0 # records how many values from the dropbox have been submitted
+thermo_submits = 0
+#vals_submitted = 0 # records how many values from the dropbox have been submitted
+#units_submitted = 0
+
+def open_options_window():
+     options_win = Toplevel(root)
+     options_win.title("Unit Converter - Options Page")
+     options_win.geometry(f"800x500+{center_x}+{center_y}")
+     about_text = Label(options_win,
+                        text="This Python program was created by Josh Hutchings and Jersey Wilden:\n\nJosh - formula calculations and unit conversions\nJersey - graphical user interface (GUI)",
+                        font=(15), justify="left") # creates a label containing the current submission choice
+     about_text.place(x=50,y=100) # places the label a factor of 20 below the previous entry
+
+     options_goback_button = Button(options_win, text="GO BACK", font=("Comic Sans MS", 12), anchor="center", padx=88, pady=10,
+                     activebackground="white", activeforeground="grey", bg="red", fg="white", cursor="hand2", command=options_win.destroy)
+     options_goback_button.place(x=275, y=350)
 
 
 def open_converter_window():
@@ -26,22 +41,67 @@ def open_converter_window():
 ##     output_log.add(right_list)
 ##     output_log.pack(fill=BOTH, expand=True)
 
-     def thermo_val_submitted():
-          global vals_submitted
-          print("val count =", vals_submitted)
-          log_text = "Value chosen: " + thermo_val_choice.get()
+     def thermo_submitted():
+          global thermo_submits
+          #print("thermo count =", thermo_submits)
+          thermo_unit_str = thermo_unit_choice.get()
+          first_brack_ind = thermo_unit_str.find("(")
+          second_brack_ind = thermo_unit_str.find(")")
+          #print(first_brack_ind)
+          #print(second_brack_ind)
+          unit_abbrev = thermo_unit_str[first_brack_ind+1:second_brack_ind]
+          log_text = thermo_val_choice.get() + " of " + thermo_num_choice.get() + " " + unit_abbrev
           output_log = Label(converter_win, text=log_text) # creates a label containing the current submission choice
-          output_log.place(x=450,y=(95+vals_submitted*20)) # places the label a factor of 20 below the previous entry
-          vals_submitted += 1 # adds 1 to the count since 1 more submission has been made
+          output_log.place(x=475,y=(95+thermo_submits*20)) # places the label a factor of 20 below the previous entry
+          thermo_submits += 1 # adds 1 to the count since 1 more submission has been made
+
+##     def thermo_val_submitted():
+##          global vals_submitted
+##          print("val count =", vals_submitted)
+##          log_text = "Value chosen: " + thermo_val_choice.get()
+##          output_log = Label(converter_win, text=log_text) # creates a label containing the current submission choice
+##          output_log.place(x=475,y=(95+vals_submitted*20)) # places the label a factor of 20 below the previous entry
+##          vals_submitted += 1 # adds 1 to the count since 1 more submission has been made
+##
+##     def thermo_unit_submitted():
+##          global units_submitted
+##          print("unit count =", units_submitted)
+##          log_text = "Value chosen: " + thermo_unit_choice.get()
+##          output_log = Label(converter_win, text=log_text) # creates a label containing the current submission choice
+##          output_log.place(x=475,y=(95+units_submitted*20)) # places the label a factor of 20 below the previous entry
+##          units_submitted += 1 # adds 1 to the count since 1 more submission has been made
+     
+     thermo_num_choice = StringVar()
+     thermo_num_entry = Entry(converter_win, font=(10), textvariable=thermo_num_choice)
+     thermo_num_entry.insert(2, "Enter a number")
+     thermo_num_entry.place(x=100, y=50)   
+     
+     thermo_submit_button = Button(converter_win, text="Submit", font=(8), padx=0, pady=0, command=thermo_submitted)
+     thermo_submit_button.place(x=175, y=200)
+
 
      thermo_val_choice = StringVar()
      thermo_val_dropdown = ttk.Combobox(converter_win, font=(10), textvariable=thermo_val_choice)
-     thermo_val_dropdown["values"] = ("Choose a value", "Energy", "Frequency", "Wavelength", "Wavenumber","Time", "Mass", "Velocity", "Momentum", "Charge", "Voltage")
+     thermo_val_dropdown["values"] = ("Energy", "Frequency", "Wavelength", "Wavenumber")
      thermo_val_dropdown.set("Choose a value")
      thermo_val_dropdown.place(x=100, y=100)   
      
-     thermo_val_button = Button(converter_win, text="Submit", font=(8), padx=0, pady=0, command=thermo_val_submitted)
-     thermo_val_button.place(x=350, y=95)
+     #thermo_val_button = Button(converter_win, text="Submit", font=(8), padx=0, pady=0, command=thermo_val_submitted)
+     #thermo_val_button.place(x=375, y=95)
+
+     thermo_unit_choice = StringVar()
+     thermo_unit_dropdown = ttk.Combobox(converter_win, font=(10), textvariable=thermo_unit_choice)
+     thermo_unit_dropdown["values"] = ("Joules (J)", "Electron volts (eV)", "Nanometers (nm)", "Meters (m)", "Hertz (Hz)", "Kilohertz (KHz)")
+     thermo_unit_dropdown.set("Choose a unit")
+     thermo_unit_dropdown.place(x=100, y=150)   
+     
+     #thermo_unit_button = Button(converter_win, text="Submit", font=(8), padx=0, pady=0, command=thermo_unit_submitted)
+     #thermo_unit_button.place(x=375, y=145)
+
+     converter_goback_button = Button(converter_win, text="GO BACK", font=("Comic Sans MS", 12), anchor="center", padx=88, pady=10,
+                     activebackground="white", activeforeground="grey", bg="red", fg="white", cursor="hand2", command=converter_win.destroy)
+     converter_goback_button.place(x=275, y=350)
+
 
 
 root = Tk()
@@ -65,7 +125,7 @@ exit_button = Button(root, text="EXIT", font=("Comic Sans MS", 12), anchor="cent
 exit_button.place(x=300, y=250)
 
 options_button = Button(root, text="OPTIONS", font=("Comic Sans MS", 12), anchor="center", padx=28.5, pady=1,
-                        activebackground="white", activeforeground="grey", bg="blue", fg="white", cursor="hand2")
+                        activebackground="white", activeforeground="grey", bg="blue", fg="white", cursor="hand2", command=open_options_window)
 options_button.place(x=335, y=325)
 
 root.mainloop()
