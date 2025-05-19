bc3013365bc836e4b295c4825fe9d74de7ff790d
Blue Moon <j.hutchings-23@student.lboro.ac.uk>
2025-05-19 21:12:05 +0100

    changed driver to work

--- driver.py
+++ driver.py
@@ -3,12 +3,14 @@
 
 print("*** Starting the driver script ***\n")
 
-import Jersey.GUI as gui
-print("Imported modules successfully.")
 import Josh.quantum_energy as qe
 
 print("Imported modules successfully.")
+
+import Jersey.GUI as gui
+print("Imported modules successfully.")
+
 print("Calling Output...\n")
 
 qe.Output("Energy", 1.77, "eV")
-
+gui.open_home_window()
